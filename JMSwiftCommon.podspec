4c0e99d9ceddf27bda261722abd2914dc0c06b01
gengrj <gengruijie@yunniao.me>
2023-06-21 10:41:46 +0800

    0.1.9

--- JMSwiftCommon.podspec
+++ JMSwiftCommon.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JMSwiftCommon'
-  s.version          = '0.1.8'
+  s.version          = '0.1.9'
   s.summary          = 'A short description of JMSwiftCommon.'
 
 # This description is used to generate tags and improve search results.
@@ -37,7 +37,7 @@ TODO: Add long description of the pod here.
   s.dependency  'SnapKit', '~> 4.2.0'
   
   s.resource_bundles = {
-      'JMVideoPlayer' => ['JMVideoPlayer/Assets/*.{xcassets}']
+      'JMSwiftCommon' => ['JMSwiftCommon/Assets/*.{xcassets}']
   }
   
   # s.resource_bundles = {
--- JMSwiftCommon/Classes/VideoPlayer/Tools/HTImageHelper.swift
+++ JMSwiftCommon/Classes/VideoPlayer/Tools/HTImageHelper.swift
@@ -13,12 +13,12 @@ public class HTImageHelper: NSObject {
     public static func imageWithName(_ name: String) -> UIImage? {
         
        var bundle: Bundle?
-       if let url = Bundle.main.url(forResource: "JMVideoPlayer.xcassets", withExtension: nil) {
+       if let url = Bundle.main.url(forResource: "JMSwiftCommon.xcassets", withExtension: nil) {
            
            bundle = Bundle(url: url)
        }else {
            
-           bundle = HTImageHelper.frameworkBundle("JMVideoPlayer")
+           bundle = HTImageHelper.frameworkBundle("JMSwiftCommon")
        }
        
        let image = UIImage.init(named: name, in: bundle, compatibleWith: nil)
